f9356e1a93a1ec3d065ec2fe235e55dd105d0ffb
jyotirsai <jyotirsai28@gmail.com>
2020-09-27 12:42:20 -0600

    animations

--- components/Terminal.js
+++ components/Terminal.js
@@ -1,5 +1,6 @@
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import { motion } from "framer-motion";
 
 const useStyles = makeStyles({
   container: {
@@ -7,7 +8,7 @@ const useStyles = makeStyles({
     direction: "column",
     alignItems: "center",
     justifyContent: "center",
-    height: "100vh",
+    height: "95vh",
   },
   text: {
     color: "white",
@@ -20,10 +21,19 @@ export default function Terminal() {
   const classes = useStyles();
   return (
     <div className={classes.container}>
-      <Typography display="block" variant="body1" className={classes.text}>
-        Hello! I'm Jyotir. <br /> A mechanical engineering student <br /> and
-        self-taught programmer.
-      </Typography>
+      <motion.div
+        animate={{
+          opacity: ["0", "1"],
+          scale: [2, 1],
+        }}
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.75 }}
+      >
+        <Typography display="block" variant="body1" className={classes.text}>
+          Hello! I'm Jyotir. <br /> A mechanical engineering student <br /> and
+          self-taught programmer.
+        </Typography>
+      </motion.div>
     </div>
   );
 }
